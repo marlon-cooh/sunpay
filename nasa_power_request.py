2e5b6f971a5038b2ecff539c12c7d4ef808fea79
Marlon Marin <marlonmariin97@outlook.com>
2025-05-22 15:10:40 -0500

    (build) nasa_power_request.py > request_parameters() function allows to fetch data from NASAMonthlyAPI, output dataframe still with missing values

--- nasa_power_request.py
+++ nasa_power_request.py
@@ -1,24 +1,11 @@
 import requests #type: ignore
 import pandas as pd #type: ignore
-import json, timeit
+import timeit
 
 parameters = [
     'CLRSKY_SFC_SW_DWN',
     'ALLSKY_SFC_SW_DWN',
     'ALLSKY_KT',
-    'WS2M',
-    'WS10M',
-    'WS50M',
-    'T2M',
-    'T10M',
-    'TS',
-    'QV2M',
-    'RH2M',
-    'PRECTOTCORR',
-    'PS',
-    'WD2M',
-    'WD10M',
-    'WD50M',
     'ALLSKY_SFC_UV_INDEX',
     'ALLSKY_SRF_ALB',
     'ALLSKY_SFC_SW_UP',
@@ -41,42 +28,60 @@ parameters = [
     'CLRSKY_SFC_SW_DNI',
     'CLRSKY_SFC_SW_UP',
     'CLOUD_OD',
-    'U2M',
-    'U10M',
-    'U50M',
-    'MIDDAY_INSOL',
-    'V2M',
-    'V10M',
-    'V50M',
-    'PBLTOP',
-    'PW',
-    'SLP',
-    'RHOA',
-    'ZENITH_LUMINANCE'
 ]
 
-parameters_subset = parameters[1]
-lon, lat = 3.997587, -73.756138
-date_range = ['20190101', '20190301']
+def request_parameters(params, lon, lat, date_range=[2020, 2024]) -> pd.DataFrame:
+    """
+        Returns a cleaned dataset of the parameters fetched from the NASA PowerLarc database.
+            Arguments:
+                * params: list of parameters to be fetched from the database.
+                * lon: longitude of the location.
+                * lat: latitude of the location.
+                * date_range: list of dates to be fetched from the database, in this case set between 2020-01 and 2024-01..
+            Returns:
+                * dt : dataset of params along a specified time, in this case set between 2020-01 and 2024-01.
+    """
+   
+    # API URL request, output in JSON format.
+    api = f'https://power.larc.nasa.gov/api/temporal/monthly/point?start={date_range[0]}&end={date_range[1]}&latitude={lat}&longitude={lon}&community=re&parameters={params}&format=JSON&header=false&time-standard=utc'
 
-daily_api = f'https://power.larc.nasa.gov/api/temporal/monthly/point?parameters={parameters_subset}&community=RE&longitude={lon}&latitude={lat}&start={date_range[0]}&end={date_range[1]}&format=JSON'
-monthly_api = f'https://power.larc.nasa.gov/api/temporal/monthly/point?parameters={parameters_subset}&community=RE&longitude={lon}&latitude={lat}&format=CSV&start=2020&end=2021'
-test_api = f"https://power.larc.nasa.gov/api/temporal/monthly/point?start=2022&end=2024&latitude={lat}&longitude={lon}&community=re&parameters={parameters_subset}&format=csv&header=true&time-standard=utc"
+    # GET request to the API.
+    response = requests.get(api).json()
+    # Extract the data from the response in JSON format.
+    json_response = response["properties"]["parameter"][params]
+    # Removing annual average values that can not be interpreted with datetime methods.
+    clean_dict = {k:v for k, v in json_response.items() if not k.endswith('13')}
+    
+    # Convert the dictionary to a DataFrame.
+    df = pd.DataFrame(
+        {
+            "date" : clean_dict.keys(),
+            "value" : clean_dict.values()
+        }
+    )
+    
+    df['date'] = pd.to_datetime(df['date'], format='%Y%m')
+    df['month'] = df['date'].dt.month
+    
+    # Show dataset for analysis.
+    df = df[['month', 'value']]
+    return df
 
-def make_request():
-    return requests.get(test_api).text
-
-# # Measure the time taken for the API request
-# execution_time = timeit.timeit(make_request, number=1)
-# print(f"API request took {execution_time:.2f} seconds")
-
-response = make_request()
-# irradiance_from_rq = response["properties"]["parameter"][parameters_subset]
-# df = pd.DataFrame({
-#     "date": list(irradiance_from_rq.keys()),
-#     "irradiance": list(irradiance_from_rq.values())
-# })
-# df["date"] = pd.to_datetime(df["date"])
-# print(df)
-
-print(response)
+if __name__ == "__main__":
+    
+    exec_time = timeit.timeit(
+        'request_parameters(params=parameters[1], lon=3.997587, lat=-73.756138)',
+        globals=globals(),
+        number=1
+    )
+    
+    print(f"Execution time: {exec_time:.4f} seconds")
+    
+    # dataset = request_parameters(
+    #     params=parameters[1],
+    #     lon=3.997587,
+    #     lat=-73.756138
+    #     # date_range=[2020, 2024]
+    # )
+    
+    # print(dataset.head())
