093c8846a75bde0abfc4c5509eb243f4791f1b32
Marlon Marin <marlonmariin97@outlook.com>
2025-05-22 20:26:37 -0500

    (build) new functions added aimed to calculate key outputs of sizing a PV system. Figures seem to be similar to those given by PVSyst (Meteonorm) and Global Solar Atlas.

--- nasa_power_request.py
+++ nasa_power_request.py
@@ -1,6 +1,6 @@
-import requests #type: ignore
+import requests, timeit #type: ignore
 import pandas as pd #type: ignore
-import timeit
+import numpy as np #type: ignore
 
 parameters = [
     'CLRSKY_SFC_SW_DWN',
@@ -63,25 +63,120 @@ def request_parameters(params, lon, lat, date_range=[2020, 2024]) -> pd.DataFram
     df['date'] = pd.to_datetime(df['date'], format='%Y%m')
     df['month'] = df['date'].dt.month
     
-    # Show dataset for analysis.
-    df = df[['month', 'value']]
-    return df
+    # Convert any negative values of fetched param to NaN.
+    df.loc[df['value'] <= 0, 'value'] = np.nan
+    
+    # Removing missing values.
+    df.dropna(inplace=True)
+    return df.value.mean()
+
+def pvout(yearly_irr, pr=0.8):
+    """
+        Calculates the output of the PV system.
+            Arguments:
+                * yearly_irr: yearly irradiation in kWh/m2/year.
+                * pr: performance ratio of the PV system.
+            Returns:
+                * pvout: output of the PV system in kWh/kWp/year.
+    """    
+    # Output of the PV system in kWh/kWp/year.
+    pvout = yearly_irr * pr
+    return pvout
+
+def required_peak_power(monthly_value, pvout):
+    """
+        Sizes the PV system based on the monthly value of the parameter.
+            Arguments:
+                * monthly_value: monthly value of the parameter in kWh/month.
+                * pvout: output of the PV system in kWh/kWp/year.
+            Returns:
+                * peak_power: size of the PV system in kWp.
+    """
+    yearly_value = monthly_value * 12
+    peak_power = yearly_value / pvout
+    return peak_power
+
+def no_of_panels(peak_power, nominal_power=0.620):
+    """
+        Calculates the number of panels required for the PV system.
+            Arguments:
+                * peak_power: size of the PV system in kWp.
+                * nominal_power: nominal power of the panel in kWp.
+            Returns:
+                * no_of_panels: number of panels required for the PV system.
+
+    """
+    no_of_panels = peak_power / nominal_power
+    return no_of_panels
+
+def energy_produced(yearly_irr, efficiency, ind_area=2.53, pr=0.8):
+    """
+        Calculates the energy produced by the PV system.
+            Arguments:
+                * yearly_irr: yearly irradiation in kWh/m2/year.
+                * efficiency: efficiency of the PV system.
+            Returns:
+                * energy_produced: energy produced by the PV system in kWh/year.
+    """
+    # Area of the panel array in m2.
+    area = no_of_panels * ind_area
+    
+    # Energy produced by the PV system in kWh/year.
+    energy_produced = yearly_irr * efficiency * area * pr
+    return energy_produced
+
+def fetch_request_parameters_time(runs):
+    """
+    Benchmarks the execution time of the request_parameters function.
+    Arguments:
+        * runs: Number of times to run the benchmark.
+    """
+    total_time = 0
+    for i in range(runs):
+        exec_time = timeit.timeit(
+            'request_parameters(params=parameters[1], lon=3.997587, lat=-73.756138)',
+            globals=globals(),
+            number=1
+        )
+        total_time += exec_time
+        print(f"Run {i+1}: {exec_time:.4f} seconds")
+    average_time = total_time / runs
+    print(f"\nAverage execution time over {runs} runs: {average_time:.4f} seconds")
 
 if __name__ == "__main__":
     
-    exec_time = timeit.timeit(
-        'request_parameters(params=parameters[1], lon=3.997587, lat=-73.756138)',
-        globals=globals(),
-        number=1
+    # Inputs.
+    # Location coordinates.
+    dataset = request_parameters(
+        params=parameters[1],
+        lon=3.997587,
+        lat=-73.756138
+        # date_range=[2020, 2024]
     )
     
-    print(f"Execution time: {exec_time:.4f} seconds")
+    # User consumption.
+    user_consumption = 1000 # kWh/month
     
-    # dataset = request_parameters(
-    #     params=parameters[1],
-    #     lon=3.997587,
-    #     lat=-73.756138
-    #     # date_range=[2020, 2024]
-    # )
+    yearly_irr = dataset * 365.25
+    # Calculate the output of the PV system.
+    pvout = pvout(yearly_irr)
     
-    # print(dataset.head())
+    # Calculate the size of the PV system.
+    peak_power = required_peak_power(user_consumption, pvout)
+    
+    # Calculate the number of panels required for the PV system.
+    no_of_panels = no_of_panels(peak_power)
+    
+    # Calculate the energy produced by the PV system.
+    energy_produced = energy_produced(yearly_irr, efficiency=0.2315)
+    
+    # Print the results.
+    print(f"User consumption: {user_consumption} kWh/month")
+    print(f"Yearly irradiation: {yearly_irr:.2f} kWh/m2/year")
+    print(f"Output of the PV system: {pvout:.2f} kWh/kWp/year")
+    print(f"Size of the PV system: {peak_power:.2f} kWp")
+    print(f"Number of panels required: {no_of_panels:.2f}")
+    print(f"Energy produced by the PV system: {energy_produced:.2f} kWh/year")
+    
+    
+    
